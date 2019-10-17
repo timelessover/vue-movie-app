72f6b9432444b2bd9504016502de253a9c811f6e
YanPing Liu <yanping.liu@questmobile.com.cn>
2019-10-17 19:01:02 +0800

    打包上线

--- app/vue.config.js
+++ app/vue.config.js
@@ -5,8 +5,9 @@ function resolve(dir) {
 }
 
 module.exports = {
+    publicPath: './',
     // 生产环境是否生成 sourceMap 文件
-    productionSourceMap: false,
+    productionSourceMap: true,
     // 开启 CSS source maps?
     css:{
         sourceMap: false,
